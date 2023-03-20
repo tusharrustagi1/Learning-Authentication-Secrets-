96c76ba829f0d92c78f2c94b92175be18edf4bde
TusharRustagi <tushar.rustagi16@gmail.com>
2023-03-20 18:52:50 +0530

    Add Environment Variables

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const app=express();
 const mongoose=require('mongoose');
 const encrypt=require('mongoose-encryption');
+console.log(process.env.API_KEY);
 app.use(express.static("public"));
 app.set("view engine",'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,8 +14,7 @@ const userSchema=new mongoose.Schema({
     email:String,
     password:String
 });
-const secret="Thisisourlittlesecret."
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
 const User=new mongoose.model("User",userSchema);
 app.get("/",function(req,res){
     res.render("home");
