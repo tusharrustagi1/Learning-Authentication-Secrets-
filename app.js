37edf90d9a7960e72c0b331ec98511c2130c503e
TusharRustagi <tushar.rustagi16@gmail.com>
2023-03-23 17:16:08 +0530

    Oauth google added

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose=require('mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocalMongoose=require('passport-local-mongoose');
+const GoogleStrategy=require('passport-google-oauth20').Strategy;
+const findOrCreate=require('mongoose-findorcreate');
+const FacebookStrategy=require('passport-facebook');
 app.use(express.static("public"));
 app.set("view engine",'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -16,13 +19,82 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB",{usenewUrlParser:true});
 const userSchema=new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String
 });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User=new mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+});
+passport.deserializeUser(function(user, cb) {
+process.nextTick(function() {
+    return cb(null, user);
+});
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+    async function(accessToken,refreshToken,profile,done)
+    {
+        console.log(profile);
+        try{
+            let user=await User.findOne({googleId:profile.id});
+            if(!user)
+            {
+                const username=Array.isArray(profile.emails)&&profile.emails.length>0?profile.emails[0].value.split("@")[0]:'';
+                const newUser=new User({
+                    username:profile.displayName,
+                    googleId:profile.id
+                });
+                user=await newUser.save();
+            }
+            return done(null,user);
+        }
+        catch(err)
+        {
+            return done(err);
+        }
+    }
+  ));
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+    },
+    async function(accessToken,refreshToken,profile,done)
+    {
+        console.log(profile);
+        try{
+            let user=await User.findOne({facebookId:profile.id});
+            if(!user)
+            {
+                const username=Array.isArray(profile.emails)&&profile.emails.length>0?profile.emails[0].value.split("@")[0]:'';
+                const newUser=new User({
+                    username:profile.displayName,
+                    facebookId:profile.id
+                });
+                user=await newUser.save();
+            }
+            return done(null,user);
+        }
+        catch(err)
+        {
+            return done(err);
+        }
+    }
+));
 app.get("/",function(req,res){
     res.render("home");
 });
@@ -57,6 +129,15 @@ app.get("/logout",function(req,res)
         }
     });
 });
+app.get("/auth/google",passport.authenticate("google",{scope:['profile']}));
+app.get('/auth/google/secrets', passport.authenticate('google', { failureRedirect: '/login' }),function(req, res) {
+    res.redirect('/secrets');
+  });
+  app.get('/auth/facebook',passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',passport.authenticate('facebook', { failureRedirect: '/login' }),function(req, res) {
+    res.redirect('/secrets');
+  });
 app.post("/register",function(req,res)
 {
     User.register({username:req.body.username},req.body.password,function(err,user)
